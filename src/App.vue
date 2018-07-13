34cc0834364b8b2d3d50c24e04cd753fe62eea27
zhanghf <zhanghf_123@sina.com>
2018-07-14 02:10:04 +0800

    demo

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <headNav></headNav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import headNav from '@/components/common/headNav'
+export default {
+  name: 'App',
+  components:{
+    headNav
+  }
+}
+</script>
+
+<style>
+@import "../static/css/global.css";
+@import "../static/css/letGlobal.less";
+</style>
---
+++ src/components/photoshare/photo.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="mb">
+        <head-nav title="图片详情"></head-nav>
+        <div class="photoImgs">
+            <img class="preview-img" height="300" :key="index"  v-for="(item,index) in picList"  :src="item.picUrl"  @click="$preview.open(index,picList)" v-lazy="item.picUrl"  />
+        </div>
+    </div>
+</template>
+
+<script>
+    import 'photoswipe/dist/photoswipe.css'
+    export default {
+        data() {
+            return {
+                picList:[]
+            }
+        },
+        created(){
+
+        },
+        methods:{
+            async handleClose(data){
+                data.forEach((ele)=>{
+                    ele.w = 300;
+                    ele.h = 300;
+                    ele.msrc = ele.picUrl
+                })
+                this.picList = data;
+
+            }
+        }
+    }
+</script>
+
+<style lang="less">
+    @rem:750/10rem;
+    .mb{
+        margin-bottom: 130/@rem;
+    }
+    .photoImgs{
+        padding:40/@rem 40/@rem 0;
+        background: white;
+        position: relative;
+        img{
+            display: block;
+            width: 670/@rem;
+            margin-bottom: 30/@rem;
+            object-fit:cover;
+        }
+    }
+</style>
